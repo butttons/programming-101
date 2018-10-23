0aa2cdd1f45882fc8b1d037b564f90178b7b71a3
Yash Baheti <zilllionaire@gmail.com>
2018-10-23 10:23:49 +0530

    changed dir structure

--- src/.vuepress/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    title: 'Programming 101',
-    themeConfig: {
-        nav: [ { text: 'Home', link: '/' }, { text: 'Dev setup', link: '/dev-setup/' }, { text: 'Resources', link: '/resources/' } ],
-        sidebar: 'auto'
-    }
-};
--- src/README.md
+++
@@ -1,6 +0,0 @@
-# Intro to programming
-
-Hello! These are a collection of useful tutorials and libararies.
-## [Resource List](/resources/)
-## [Development Setup](/dev-setup/)
-## [Tutorials](/tutorials/)
